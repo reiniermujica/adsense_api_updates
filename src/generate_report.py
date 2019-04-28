4deceb5cce2d8fad95d96f5405524b86a0309107
Reinier Mujica <reinier.mujica@gmail.com>
2019-04-28 15:18:25 +0200

    frist mock

--- src/generate_report.py
+++ src/generate_report.py
@@ -1,62 +1,65 @@
 #!/usr/bin/python
 
-"""This example retrieves a report for the specified ad client.
-
-Please only use pagination if your application requires it due to memory or
-storage constraints.
-If you need to retrieve more than 5000 rows, please check generate_report.py,
-as due to current limitations you will not be able to use paging for large
-reports. To get ad clients, run get_all_ad_clients.py.
-
-Tags: reports.generate
-"""
-
-__author__ = 'jalc@google.com (Jose Alcerreca)'
 
 import argparse
 import sys
 from apiclient import sample_tools
 from oauth2client import client
-# from adsense_util import fill_date_gaps
+
 from adsense_util import get_account_id
+
+from datetime import datetime
 
 MAX_PAGE_SIZE = 10
 # This is the maximum number of obtainable rows for paged reports.
 ROW_LIMIT = 5000
 
-# Declare command-line flags.
-argparser = argparse.ArgumentParser(add_help=False)
-argparser.add_argument(
-    'ad_client_id',
-    help='The ID of the ad client for which to generate a report')
+
+def get_month_start():
+    now = datetime.today().replace(day=1)
+
+    day = now.day
+    month = now.month
+    year = now.year
+
+    return "{}-{:02d}-{:02d}".format(year, month, day)
+
+
+def get_month_end():
+    now = datetime.today()
+
+    day = now.day
+    month = now.month
+    year = now.year
+
+    return "{}-{:02d}-{:02d}".format(year, month, day)
 
 
 def main(argv):
     # Authenticate and construct service.
     service, flags = sample_tools.init(
-        argv, 'adsense', 'v1.4', __doc__, __file__, parents=[argparser],
+        argv, 'adsense', 'v1.4', __doc__, __file__, parents=[],
         scope='https://www.googleapis.com/auth/adsense.readonly')
 
-    ad_client_id = flags.ad_client_id
-
     try:
-        # Let the user pick account if more than one.
         account_id = get_account_id(service)
 
-        # Retrieve report in pages and display data as we receive it.
+        start_date = get_month_start()
+        end_date = get_month_end()
+
         start_index = 0
         rows_to_obtain = MAX_PAGE_SIZE
         result_all_pages = None
         while True:
             result = service.reports().generate(
-                accountId='pub-3136901199458158',
-                startDate='today-1w', endDate='today',
-                filter=['AD_CLIENT_ID==ca-pub-3136901199458158'],
-                metric=['PAGE_VIEWS', 'AD_REQUESTS', 'AD_REQUESTS_COVERAGE',
+                accountId=account_id,
+                startDate=start_date, endDate=end_date,
+                filter=['AD_CLIENT_ID==ca-' + account_id],
+                metric=['PAGE_VIEWS', 'AD_REQUESTS',
                         'CLICKS', 'AD_REQUESTS_CTR', 'COST_PER_CLICK',
                         'AD_REQUESTS_RPM', 'EARNINGS'],
-                dimension=['DATE'],
-                sort=['+DATE'],
+                dimension=['AD_UNIT_CODE', 'AD_UNIT_NAME'],
+                useTimezoneReporting=True,
                 startIndex=start_index,
                 maxResults=rows_to_obtain).execute()
 
@@ -89,7 +92,6 @@ def print_result(result):
         print('%25s' % header['name'])
     print
 
-    # Display results for this page.
     if 'rows' in result:
         for row in result['rows']:
             for column in row:
